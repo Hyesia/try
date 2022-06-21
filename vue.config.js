c4ae50a8f725f1e7e47fe96a35043042128ef738
Hyesiab <YangAdam98@yahoo.fr>
2022-06-21 15:44:07 +0200

    my modif

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
+const publicPath = process.env.NODE_ENV === 'production' ? '/try/' : '/' // <-- add this line
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+	publicPath: publicPath,
   transpileDependencies: true
 })
